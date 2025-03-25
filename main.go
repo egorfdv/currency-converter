a82576803e14537758164f67d353c354d4bacf50
egorfdv <egorfdvru@gmail.com>
2025-03-25 18:51:52 +0300

    Adding a function to read the input of the amount to convert from the user

--- main.go
+++ main.go
@@ -6,5 +6,14 @@ func main() {
 	fromDollarToEuro := 0.92
 	fromDollarToRuble := 84.19
 	fromEuroToRuble := fromDollarToRuble / fromDollarToEuro
-	fmt.Printf("%.2f", fromEuroToRuble)
+	fmt.Printf("%.2f\n", fromEuroToRuble)
+	userInputSum := userInputSum()
+	fmt.Println(userInputSum)
+}
+
+func userInputSum() float64 {
+	var userInputSum float64
+	fmt.Print("Enter the sum to convert: ")
+	fmt.Scan(&userInputSum)
+	return userInputSum
 }
