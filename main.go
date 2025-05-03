32fc87c34a5557d0a90fa52ccaabea1b98c61e7e
egorfdv <egorfdvme@gmail.com>
2025-05-03 12:39:52 +0300

    create constants

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const USDToEUR = 0.88
+	const USDToRUB = 81.49
+	const EURToRUB = USDToRUB / USDToEUR
+	fmt.Println(USDToEUR, USDToRUB, EURToRUB)
 }
